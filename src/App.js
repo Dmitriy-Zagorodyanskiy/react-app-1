fe3a27476b85bffa971a1aa80ebf634343a6f2cf
dimzag897@gmail.com <dimzag897@gmail.com>
2025-12-31 14:01:26 +0200

    feat: some changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 import React, { useState } from 'react';
+import './styles/App.css';
 
 function App() {
-  const [likes, setLikes] = useState(0);
   const [value, setValue] = useState('TEXT IN INPUT');
-
-  
-  function increment() {
-    setLikes(likes + 1);
-  }
-
-  function decrement() {
-    setLikes(likes - 1);
-  }
 
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input
-        type='text'
-        value={value}
-        onChange={event => setValue(event.target.value)}
-      />
-      <button onClick ={increment}>Increment</button>
-      <button onClick ={decrement}>Decrement</button>
+      <div className='post'>
+        <div className='post__content'>
+          <strong> 1. JS </strong>
+          <div>
+            JS JS JS
+          </div>
+        </div>
+        <div className='post__btns'>
+          <button> Delete button </button>
+        </div>
+      </div>
     </div>
   );
 }
